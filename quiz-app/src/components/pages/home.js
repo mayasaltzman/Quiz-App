69a6800ca23296d947069e66f2ac02187ee94d9e
Maya Saltzman <mayasaltzman3@gmail.com>
2024-01-24 16:18:27 -0500

    added some basic styles to qquestions

--- quiz-app/src/components/pages/home.js
+++ quiz-app/src/components/pages/home.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div>
-            <h1>University of Guelph Quiz App</h1>
+            <h1>University of Guelph Quiz</h1>
+            <h2>Can you answer these trivia questions about the University of Guelph?</h2>
             <Link to = "/Quiz"><button>Start Quiz</button></Link>
         </div>
     )
